ffcecaf07544b61b371777d4c9af259844f19d7b
Rafsan <rafsan.quayes@gmail.com>
2021-04-30 10:15:10 +0600

    Fixed file issue

--- loadContent.js
+++ loadContent.js
@@ -1,3 +1,13 @@
 const fs = require("fs");
 
-fs.readFileSync("./contents/about.html");
+
+
+fs.readFile('i:/PoraLekha/5/Server Progrmming Lab/Lab_1_180042141/contents/avout.js', (err,data)=>{
+    if(err){
+        console.log("error:");
+        console.log(err);
+    } else {
+        console.log("read successful\n" + data);
+    }
+    
+});
