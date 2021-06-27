a0fd47ab81d4ac5f6762a1e45339fb3a842ee7f0
Rafsan <rafsan.quayes@gmail.com>
2021-06-28 00:01:55 +0600

    Server works

--- Lab_02+03/app.js
+++ Lab_02+03/app.js
@@ -2,7 +2,15 @@ const express = require('express');
 const app = express();
 
 app.get("/", (req, res)=>{
-    res.send("<H1>Home Page</H1>")
+    res.send("<H1>Home Page</H1>");
+})
+
+app.get("/about", (req, res)=>{
+    res.json({Name: "John", Age: "41", Profession: "None"})
+})
+
+app.use((req,res)=>{
+    res.status(401).send("Page does not exist");
 })
 
 module.exports = app;
