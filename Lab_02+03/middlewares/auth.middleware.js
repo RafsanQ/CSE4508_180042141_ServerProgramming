ba32b8ea2b5a318ae74632f5bb2e6909af95a637
Rafsan <rafsan.quayes@gmail.com>
2021-07-02 13:40:56 +0600

    Login checks with hashed value

--- Lab_02+03/middlewares/auth.middleware.js
+++ Lab_02+03/middlewares/auth.middleware.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const mysql = require('mysql');
+const bcrypt = require('bcrypt')
 
 const isLoggedIn = (req, res, next)=>{
 
     const email = req.body.email;
     const password = req.body.password;
+
 
     // Create a connection to the mysql database
     const db = mysql.createConnection({
@@ -16,19 +18,32 @@ const isLoggedIn = (req, res, next)=>{
 
     const sqlQuery = "SELECT Email, Password FROM users WHERE Email='" + email + "'";
 
+    
+
     db.query(sqlQuery, (err, result) => {
         if(err) throw err;
         console.log(result);
-
-        console.log(result[0].Password + ' ' + password);
-        if(result.length >= 1 && password == result[0].Password){
-            db.release;
-            next();
+        if(result.length >= 1){
+            bcrypt.compare(password, result[0].Password, (err, result) => {
+                if(result){
+                    db.release;
+                    console.log("Login Successful")
+                    next();
+                }
+                else{
+                    res.redirect("/register");
+                }
+            })
+            
         }  
         else
             res.redirect("/register");
         db.release;
     })
+    
+    
+
+    
     
 }
 
