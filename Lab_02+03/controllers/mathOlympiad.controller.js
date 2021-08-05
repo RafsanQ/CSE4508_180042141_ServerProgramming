d5511b52467d7d5aafccdf6d566cda1ca30a78d5
Rafsan <rafsan.quayes@gmail.com>
2021-08-06 01:26:26 +0600

    Registering user checks erros.

--- Lab_02+03/controllers/mathOlympiad.controller.js
+++ Lab_02+03/controllers/mathOlympiad.controller.js
@@ -1,13 +1,59 @@
 const mathOlympiad = require('../models/mathOlymiad.model')
 
 const getMO = (req, res) => {
-    res.render('math-olympiad/register.ejs')
+    res.render('math-olympiad/register.ejs', {error:req.flash('error')})
 }
 
 const postMO = (req, res) => {
-    const {name, category, contactNo, email, institution, tshirt} = req.body;
-    console.log({name, category, contactNo, email, institution, tshirt});
-    res.render('math-olympiad/register.ejs')
+    const {name, category, contact, email, institution, tshirt} = req.body;
+    console.log({name, category, contact, email, institution, tshirt});
+
+    let registrationFee = 0;
+    if(category == 'School') registrationFee = 250;
+    else if(category == 'College') registrationFee = 400;
+    else registrationFee = 500;
+
+    const total = registrationFee;
+    const paid = 0;
+    const selected = false;
+    const date = new Date();
+    date.toLocaleDateString();
+
+    let error = "";
+
+    mathOlympiad.findOne({name:name, contact:contact}).then((participant) => {
+        if(participant){
+            error = "Participant with this name and contact already exists."
+            req.flash('error', error);
+            res.redirect('register');
+        }else{
+            const participant = new mathOlympiad({
+                name,
+                category,
+                contact,
+                email,
+                institution,
+                total,
+                paid,
+                selected,
+                tshirt,
+                date
+            });
+
+            participant
+            .save()
+            .then(()=>{
+                error = "Participant has been registered successfully.";
+                req.flash('error', error);
+                res.redirect('register');
+            })
+            .catch((e)=>{
+                error = "Unexpected error has occured";
+                console.log(e);
+                res.redirect("register")
+            })
+        }
+    })
 }
 
 const getMOList = (req, res) => {
